d5723af7c814020b308c6025d04bf272353bc64c
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2019-07-05 16:16:42 +0200

    Add ui testing scenarios

--- ActivityTimer/ActivityTimer/ActivityTimerTests/Mocks/IOS/NSManagedObjectContextMock.swift
+++ ActivityTimer/ActivityTimer/ActivityTimerTests/Mocks/IOS/NSManagedObjectContextMock.swift
@@ -1,7 +1,7 @@
 //
 //  NSManagedObjectExtension.swift
 //  ActivityTimerTests
-//
+// 
 //  Created by Krzysztof Maraszkiewicz on 23/06/2019.
 //  Copyright © 2019 Krzysztof Maraszkiewicz. All rights reserved.
 //
--- ActivityTimer/ActivityTimer/ActivityTimerUITests/ActivityTimerUITests.swift
+++ ActivityTimer/ActivityTimer/ActivityTimerUITests/ActivityTimerUITests.swift
@@ -10,25 +10,81 @@ import XCTest
 
 class ActivityTimerUITests: XCTestCase {
 
+    private var app: XCUIApplication!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        self.app = XCUIApplication()
+        
         continueAfterFailure = false
 
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_addingActivityItem() {
+        
+        let activitytimerActivitytableviewNavigationBar = app.navigationBars["ActivityTimer.ActivityTableView"]
+        activitytimerActivitytableviewNavigationBar.buttons["Add"].tap()
+        
+        let textField = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textField).element
+        textField.tap()
+        textField.typeText("testowe słowo   ")
+        textField.tap()
+        
+        print(textField.staticTexts.accessibilityElement(at: 0))
+        
+//        app.navigationBars["ActivityTimer.ActivityFormView"].buttons["Save"].tap()
+//        activitytimerActivitytableviewNavigationBar.buttons["Edit"].tap()
+//
+//        let tablesQuery = app.tables
+//
+//        XCTAssertNotNil(tablesQuery.buttons["testowe słowo"])
+//
+//        tablesQuery.buttons["testowe słowo"].tap()
+//        tablesQuery.buttons["Delete"].tap()
+//
+//        XCTAssertNil(tablesQuery.buttons["testowe słowo"])
+    }
+    
+    func test_editActivityItem() {
+        
+        let activitytimerActivitytableviewNavigationBar = app.navigationBars["ActivityTimer.ActivityTableView"]
+        activitytimerActivitytableviewNavigationBar.buttons["Add"].tap()
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textField).element.tap()
+        
+        let saveButton = app.navigationBars["ActivityTimer.ActivityFormView"].buttons["Save"]
+        saveButton.tap()
+        
+        let tablesQuery2 = app.tables
+        let tablesQuery = tablesQuery2
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["testowanie"]/*[[".cells.staticTexts[\"testowanie\"]",".staticTexts[\"testowanie\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.otherElements.containing(.navigationBar, identifier:"ActivityTimer.ActivityFormView").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textField).element.tap()
+        saveButton.tap()
+        activitytimerActivitytableviewNavigationBar.buttons["Edit"].tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.buttons["Delete testowanie 123"]/*[[".cells.buttons[\"Delete testowanie 123\"]",".buttons[\"Delete testowanie 123\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tablesQuery2.buttons["Delete"].tap()
+        
+        
+    }
+    
+    func test_deleteActivityItem() {
+        
+        let activitytimerActivitytableviewNavigationBar = app.navigationBars["ActivityTimer.ActivityTableView"]
+        activitytimerActivitytableviewNavigationBar.buttons["Add"].tap()
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textField).element.tap()
+        app.navigationBars["ActivityTimer.ActivityFormView"].buttons["Save"].tap()
+        
+        let editButton = activitytimerActivitytableviewNavigationBar.buttons["Edit"]
+        editButton.tap()
+        editButton.tap()
+        
+        let tablesQuery = app.tables
+        tablesQuery/*@START_MENU_TOKEN@*/.buttons["Delete testowanie"]/*[[".cells.buttons[\"Delete testowanie\"]",".buttons[\"Delete testowanie\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tablesQuery.buttons["Delete"].tap()
+        
     }
 
 }
