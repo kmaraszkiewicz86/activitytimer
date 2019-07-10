3c71c211037174e924386a678e54997043b0fbde
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2019-07-10 19:15:00 +0200

    Add basic configuration of cloud kit storage

---
+++ ActivityTimer/ActivityTimer/IOSShared/Services/ActivityCloudService.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityCloudService.swift
+//  IOSShared
+//
+//  Created by Krzysztof Maraszkiewicz on 10/07/2019.
+//  Copyright © 2019 Krzysztof Maraszkiewicz. All rights reserved.
+//
+
+import CloudKit
+import UIKit
+
+///Manages activity type on ICloudKit storage
+public class ActivityCloudService {
+    
+    private 
+    
+}
